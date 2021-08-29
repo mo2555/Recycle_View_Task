182d5606cce70c13b43a9a8bc6f46d99167246cc
mo2555 <medoyousef444165@gmail.com>
2021-08-29 03:08:40 +0200

    com2

--- app/src/main/java/com/example/shilongoleague/MainActivity.kt
+++ app/src/main/java/com/example/shilongoleague/MainActivity.kt
@@ -3,19 +3,23 @@ package com.example.shilongoleague
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageView
-import android.widget.ImageView.*
 import android.widget.LinearLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 class MainActivity : AppCompatActivity() {
     val dataD = ArrayList<data>()
-    val  adapter = CustomAdapter(dataD)
+    val  adapter = CustomAdapter(dataD, this,this)
     lateinit var recycler: RecyclerView
+    lateinit var imagePopup : ImageView
+    lateinit var layoutPopup : LinearLayout
     @SuppressLint("WrongConstant")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        imagePopup = findViewById(R.id.imageViewPopup)
+        layoutPopup = findViewById(R.id.layoutViewPopup)
         reData()
         reData()
         reData()
@@ -25,6 +29,12 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    fun getP(index:Int){
+     imagePopup.setImageResource(dataD[index].image)
+        layoutPopup.visibility = LinearLayout.VISIBLE
+        imagePopup.visibility = ImageView.VISIBLE
+    }
+
     fun reData(){
         dataD.add(data("Ben 2lby",R.drawable.eblaa))
         dataD.add(data("Emam",R.drawable.emam))
@@ -32,5 +42,15 @@ class MainActivity : AppCompatActivity() {
         dataD.add(data("Shika",R.drawable.shika))
         dataD.add(data("Don't cry",R.drawable.shika2))
         dataD.add(data("League",R.drawable.shilongo))
+    }
+
+    fun onClick(view: View) {
+        when(view.id){
+            imagePopup.id ,
+            layoutPopup.id -> {
+                imagePopup.visibility = ImageView.GONE
+                layoutPopup.visibility = LinearLayout.GONE
+            }
+        }
     }
 }
--- app/src/main/java/com/example/shilongoleague/custom_adapter.kt
+++ app/src/main/java/com/example/shilongoleague/custom_adapter.kt
@@ -1,29 +1,36 @@
 package com.example.shilongoleague
 
-import android.app.Activity
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
-class CustomAdapter (val dataList:ArrayList<data>):RecyclerView.Adapter<CustomAdapter.ViewHolder>(){
+class CustomAdapter(val dataList:ArrayList<data>,var context: Context, var mainActivity: MainActivity):RecyclerView.Adapter<CustomAdapter.ViewHolder>(){
     class ViewHolder(itemView: View) :RecyclerView.ViewHolder(itemView){
-    val txtName : TextView = itemView.findViewById(R.id.textView)
-    val image : ImageView = itemView.findViewById(R.id.imageView)
+        val txtName : TextView = itemView.findViewById(R.id.textView)
+        val image : ImageView = itemView.findViewById(R.id.imageView)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-    val v = LayoutInflater.from(parent.context).inflate(R.layout.view,parent,false)
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.view,parent,false)
         return ViewHolder(v)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-   var list =  dataList[position]
+        var list =  dataList[position]
         holder.txtName.text = list.name
         holder.image.setImageResource(list.image)
+        holder.image.setOnClickListener {
+            mainActivity.getP(position)
+            Toast.makeText(context,holder.txtName.text, Toast.LENGTH_LONG).show()
+        }
+        holder.txtName.setOnClickListener {
+            Toast.makeText(context,holder.txtName.text, Toast.LENGTH_LONG).show()
+        }
     }
 
     override fun getItemCount(): Int {
